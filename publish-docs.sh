2d3aea51ea972bcd571609ae729713f6d85808ce
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-03-18 00:51:40 -0700

    adding some bootstrapping

---
+++ publish-docs.sh
@@ -0,0 +1,12 @@
+# this requires sphinx to be installed:
+# $ easy_install -U sphinx
+cd docs
+make html
+git checkout gh-pages
+cd ..
+rm -rf _*
+mv _build/html/* .
+git add .
+git commit -am "build $(date)"
+git push origin gh-pages
+git checkout master
